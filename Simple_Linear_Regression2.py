3ceddb7d24a2630dcc8546516aaab9a3dfc666ff
Unknown <157136443@qq.com>
2018-05-03 17:08:52 +0800

    Simple_Linear_Regression2
    use vector operation to save time

---
+++ Simple_Linear_Regression2.py
@@ -0,0 +1,57 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+class SimpleLinearRegression2:
+
+    def __init__(self):
+        self.a_ = None
+        self.b_ = None
+
+    def fit(self, x_train, y_train):
+        '''根据训练集训练Simple Linear Regression模型'''
+        assert x_train.ndim == 1, \
+            "Simple Linear Regression can only solve single feature"
+        assert len(x_train) == len(y_train), \
+            "the size of x_train must be equal to the size of y_train"
+        x_mean = np.mean(x_train)
+        y_mean = np.mean(y_train)
+
+        num = (x_train - x_mean).dot((y_train - y_mean)) #分子
+        d = (x_train - x_mean).dot((x_train - x_mean)) #分母
+        
+        self.a_ = num / d
+        self.b_ = y_mean - self.a_ * x_mean
+        print('Fitted!')
+        return self
+    
+    def predict(self, x_predict):
+        '''给定预测数据集x_predict，返回x_predict的结果向量'''
+        assert x_predict.ndim == 1, \
+            "Simple Linear Regression can only solve single feature"
+        assert self.a_ and self.b_, \
+            "must fit before predict!"
+
+        return np.array([self._predict(x) for x in x_predict])
+
+    def _predict(self, x_single):
+        '''给定单个预测数据x_predict，返回x_predict的结果值'''
+        return self.a_ * x_single + self.b_
+
+    def __repr__(self):
+        return 'SimpleLinearRegression2()'    
+
+
+
+x = np.array([1., 2., 3., 4., 5.])
+y = np.array([1., 3., 2., 3., 5.])
+x_predict = [6, 7, 8]
+reg2 = SimpleLinearRegression2()
+reg2.fit(x, y)
+print(reg2.predict(np.array(x_predict)))
+print(reg2.a_)
+print(reg2.b_)
+y_hat2 = reg2.predict(x)
+plt.scatter(x, y)
+plt.plot(x, y_hat2, color = 'r')
+plt.axis([0, 6, 0, 6])
+plt.show()
