023c7da1c4c50bb87132f6a38fad116336e0abee
Jason <157136443@qq.com>
2018-05-12 13:25:06 +0800

    Update gradient_descent.py

--- gradient_descent.py
+++ gradient_descent.py
@@ -1,41 +1,37 @@
 import numpy as np 
 import matplotlib.pyplot as plt 
-
-#the derivative func of the original func
-def dJ(theta):
-    return 2*(theta - 2.5)
-
-#the original func
-def J(theta):
+def J(theta, X_b, y):
     try:
-        return (theta - 2.5) ** 2 - 1
+        return np.sum((y - X_b.dot(theta)) ** 2) / len(y)
     except:
         return float('inf')
 
-def gradient_descent(initial_theta, eta, epsilon = 1e-8, n_iters = 1e4):
+def dJ(theta, X_b, y):
+    return X_b.T.dot(X_b.dot(theta) - y) * 2. / len(y)
+
+def gradient_descent(X_b, y, initial_theta, eta, n_itera = 1e4, epsilon = 1e-8):
     theta = initial_theta
-    i_iter = 0
-    while i_iter < n_iters:
-        gradient = dJ(theta)
-        pre_theta = theta 
-        theta -= eta * gradient
-        theta_history.append(theta)
-        i_iter += 1
-        if abs(J(theta) - J(pre_theta)) < epsilon:
+    cur_iter = 0
+
+    while cur_iter < n_itera:
+        gradient = dJ(theta, X_b, y)
+        last_theta = theta  
+        theta = theta - eta * gradient
+        if abs(J(theta, X_b, y) - J(last_theta, X_b, y)) < epsilon:
             break
+        cur_iter += 1
+
     return theta
 
-def plot_theta_history():
-    plt.plot(plot_x, J(plot_x))
-    plt.plot(np.array(theta_history), J(np.array(theta_history)), color = 'r', marker = '*')
-    plt.show()
+m = 100000
+x = np.random.normal(size = m)
+X = x.reshape(-1, 1)
+y = 4. * x + 3. + np.random.normal(0, 3, size = m)
 
-plot_x = np.linspace(-1, 6, 141)
-plot_y = (plot_x - 2.5) ** 2 - 1
+X_b = np.hstack([np.ones((len(X), 1)), X])
+initial_theta = np.zeros(X_b.shape[1])
 eta = 0.01
-theta = 0.0
-epsilon = 1e-8
-theta_history= []
-gradient_descent(theta, eta, epsilon)
-plot_theta_history()
-print(theta_history[-1])
+
+theta = gradient_descent(X_b, y, initial_theta, eta)
+
+print(theta)
